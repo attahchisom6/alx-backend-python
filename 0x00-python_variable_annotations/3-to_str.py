40ba1ab536a212f0125bbb2f58460b5de8126e12
attahchisom6 <attahchisom@gmail.com>
2023-07-06 15:08:55 +0100

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+annotated to convert float to string
+"""
+
+
+def to_str(num: float) -> str:
+    """
+    function take a float argument and returns a str
+    """
+    return str(num)
