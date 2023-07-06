42dd03eb08eff8ec33e2646dcc8782b242c3198a
attahchisom6 <attahchisom6@gmail.com>
2023-07-06 16:31:58 +0000

    adding elements in a mixed list

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ Annontating mixed values
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """funcrion to get annotation of the sum of mixed list"""
     sum: float = 0
     for num in mxd_lst:
