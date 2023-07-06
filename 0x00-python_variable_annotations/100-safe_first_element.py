e67515a464b4d2440cbc7dd9dfff15d6bbe0757a
attahchisom6 <attahchisom6@gmail.com>
2023-07-06 20:03:10 +0000

    docker annotation safe-first

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,10 @@ from typing import Sequence, Any, Union
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    docker-Annoting a function to return either the first element in
+    the list or None
+    """
     if lst:
         return lst[0]
     else:
