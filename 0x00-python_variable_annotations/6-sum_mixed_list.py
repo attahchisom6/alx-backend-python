783ce9da938e9b54ebbcb8bf4ef627b9e9cc400d
attahchisom6 <attahchisom6@gmail.com>
2023-07-06 16:10:46 +0000

    adding elements in a mixed list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/env python3
+"""
+Annontating mixed values
+"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    sum: float = 0
+    for num in mxd_lst:
+        if type(num) is int or type(num) is float:
+            sum += num;
+    return sum
