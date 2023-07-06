5a388ef1f12cf18f93219d8e242648cd748a5490
attahchisom6 <attahchisom6@gmail.com>
2023-07-06 15:44:11 +0000

    annotating the sum of the entries in the input list to float

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 """
 annotating a function returning list
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     this function will all entries in the given list A
     and return the sum as a float
