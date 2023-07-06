05d7eb9fa3319feec6426840d25f539b8e44b537
attahchisom6 <attahchisom6@gmail.com>
2023-07-06 20:50:56 +0000

    annontating values by a dictionary

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,10 @@ from typing import Mapping, Union, Any, TypeVar
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(
+        dct: Mapping,
+        key: Any,
+        default: Union[T, None] = None) -> Union[Any, T]:
     """
     Annontating value returned by dictionary
     """
