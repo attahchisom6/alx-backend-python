7d4339f5934ef83b5f92dce827cf47004ddda9b0
attahchisom6 <attahchisom6@gmail.com>
2023-07-06 17:32:49 +0000

    to tuple

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,6 +6,7 @@ from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """this will take string, float or integer argument and return a tuple of string and float data types
+    """this will take string, float or integer argument and return
+    a tuple of string and float data types
     """
     return (k, v * v)
