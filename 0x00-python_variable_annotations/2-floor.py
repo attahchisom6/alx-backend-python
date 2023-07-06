cade85a5adafc5945f0d62271134b91618b45efa
attahchisom6 <attahchisom@gmail.com>
2023-07-06 14:24:58 +0100

    base thr float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+annontating a function to return the base value of a float
+"""
+
+
+def floor(num: float) -> int:
+    """
+    this function takes a float value and is anontated to return an int value
+    """
+    return int(num)
