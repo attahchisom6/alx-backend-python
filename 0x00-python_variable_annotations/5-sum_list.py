d51e7883e8ef3e744a3c2cc6b22996617748812d
attahchisom6 <attahchisom@gmail.com>
2023-07-06 15:52:14 +0100

    sum all entries in the list and return a float

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+annotating a function returning list
+"""
+
+
+def sum_list(A: list[float]) -> float:
+    """
+    this function will all entries in the given list A
+    and return the sum as a float
+    """
+    sum: float = 0
+    for num in A:
+        sum += num
+    return sum
