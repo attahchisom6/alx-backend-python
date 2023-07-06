f9835e630c407807eb53525969b5c246569af4b9
attahchisom6 <attahchisom6@gmail.com>
2023-07-06 18:50:36 +0000

    sequence f iterable: list of list, tuple of tuple, list of tuple

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -5,5 +5,7 @@ Annontating a sample function
 from typing import List, Tuple, Sequence, Iterable, Iterable
 
 
-def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+def element_length(lst: Iterable[Sequene]) -> List[Tuple[Sequence, int]]:
+    """annotating for sequence of iterables
+    """
     return [(i, len(i)) for i in lst]
