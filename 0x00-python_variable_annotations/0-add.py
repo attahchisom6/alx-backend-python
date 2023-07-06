c231b77acfbd404fe3f1ca4c2a51383c0938f32a
attahchisom6 <attahchisom@gmail.com>
2023-07-06 13:45:08 +0100

    advanced python

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Data annotation
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Abnotating a function to take float input and return float output
+    """
+    return float(a + b)
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+String Annontated
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Annontating function to take string input input and return string a value
+    """
+    result = "{}{}".format(str1, str2)
+    return str(result)
