6ea218d7318420e1af23df491ee204e9d3c6c0a7
attahchisom6 <attahchisom@gmail.com>
2023-07-07 18:14:46 +0100

    pseudocode

---
+++ pseudiCode-lockbox.py
@@ -0,0 +1,13 @@
+hello this is  a little insight of what i think regarding lockboxes i hope it helps
+someone. during an interview one might be called upon to answer many programming
+ questioms. believe me having a mental image of any problem is the best u can do
+for urself in an an interview as well as as in ur carrer as a software engineer code are objects!
+
+Consider a world W (list of list), where every house H (a list) is unbreakable, by any known advanced
+technology including nuclear weapon.
+In W, there are persons Mr A, B, C, D, E.
+NOTE:
+    1. if Mr A close his house with a padlock and forgets the key inside then, by hypthesis Mr A can 
+       never have access to his house Again!
+    2. Assuming mr
+
