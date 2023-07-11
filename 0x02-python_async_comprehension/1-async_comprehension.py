41f9f1ea6d8988792dcd7ece12035f4a69709a91
attahchisom6 <attahchisom6@gmail.com>
2023-07-11 11:58:16 +0000

    debugged

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,11 @@
 A coroutine rhat defines async comprehension
 """
 import asyncio
-from typing import Generator, List
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[List[float], None, None]:
+async def async_comprehension() -> List[float]:
     """
     This coroutine will collect 10 random numbers using async comprehension
     over the coroutine async generator then return the 10 random numbers
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,7 +7,7 @@ from typing import Generator
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime() -> Generator[float, None, None]:
+async def measure_runtime() -> float:
     """
     will the total time elasped in running async_comprehension 4 times
     """
