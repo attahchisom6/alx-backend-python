998fed0e7362d17690304c3ab5de99b11b03fae9
attahchisom6 <attahchisom6@gmail.com>
2023-07-30 02:30:54 +0100

    tested

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -54,19 +54,19 @@ class TestGetJson(unittest.TestCase):
 class TestMemoize(unittest.TestCase):
     """ TESTCASE """
     def test_memoize(self):
-    """ Test that when calling a_property twice, the correct result is
-    returned but a_method is on
-    ly called once using assert_called_once
-    """
-    class TestClass:
-        """ class """
-        def a_method(self):
-            """ method  """
-            return 42
-        @memoize
-        def a_property(self):
-            """ property """
-            return self.a_method()
+        """ Test that when calling a_property twice, the correct result is
+        returned but a_method is on
+        ly called once using assert_called_once
+        """
+        class TestClass:
+            """ class """
+            def a_method(self):
+                """ method  """
+                return 42
+            @memoize
+            def a_property(self):
+                """ property """
+                return self.a_method()
         with patch.object(TestClass, "a_method") as mockMethod:
             test_class = TestClass()
             test_class.a_property
